8f8824ab9383a2cfe702f8d4d744d7d7cd8606ec
de-arl <suspu@protonmail.com>
2021-02-03 03:28:51 +0100

    geändert:       sway/scripts/sway_bar.sh

--- sway/scripts/sway_bar.sh
+++ sway/scripts/sway_bar.sh
@@ -3,10 +3,10 @@
 ## Depends on iw, awk, pamixer
 ## Change values according system, PC_TYPE accepts "desktop" or random values for devices with battery, if your desktop is using wlan you may add "wlan $net $sig" in line 53
 
-CORE_COUNT=8
+CORE_COUNT=4
 WIFI_NAME="wlan0"
 PC_TYPE="desktop"
-
+#IDEV="enp5s0" #device to collect internet traffic from
 # Battery
 #bat=$(acpi -b | awk '{print $1,$3,$4,$5,$6,$7}')
 
@@ -14,8 +14,8 @@ PC_TYPE="desktop"
 #net=$(iw dev ${WIFI_NAME} link | grep SSID | awk -F ' ' '{print $2}')
 #sig=$(iw dev ${WIFI_NAME} link | grep signal |awk -F ' ' '{print $2,$3}')
 # Current session
-TX=$(( $(cat /sys/class/net/${IDEV}/statistics/tx_bytes) / 1000000 ))
-RX=$(( $(cat /sys/class/net/${IDEV}/statistics/rx_bytes) / 1000000 ))
+#TX=$(( $(cat /sys/class/net/${IDEV}/statistics/tx_bytes) / 1000000 ))
+#RX=$(( $(cat /sys/class/net/${IDEV}/statistics/rx_bytes) / 1000000 ))
 # Needs vnstat
 #TXT=$(($(vnstat --json --limit 1 | jq '.interfaces |.[]| select(.name=="enp4s0")|.traffic|.total|.tx') / 1000000000 ))
 #RXT=$(($(vnstat --json --limit 1 | jq '.interfaces |.[]| select(.name=="enp4s0")|.traffic|.total|.rx') / 1000000000 ))
@@ -55,4 +55,4 @@ upt=$(awk '{printf("%dd %02dh %02dm\n",($1/60/60/24),($1/60/60%24),($1/60%60))}'
 #	else
 #		echo  "Up $upt | CPU $mn401%-$mn410%-$mn415% | RAM $ram% | wlan $net $sig | Vol $vol% | Brightness $brp% | $bat| $dat | $hor:$min $tiz"
 fi
-echo  "Up $upt | tx/rx [Mb]: $TX/$RX | CPU $mn401%-$mn410%-$mn415% | RAM $ram% | Vol $vol% |  $(date +"%a %d. %b %Y %H:%M")    "
+echo  "Up $upt | CPU $mn401%-$mn410%-$mn415% | RAM $ram% | Vol $vol% |  $(date +"%a %d. %b %Y %H:%M")    "
